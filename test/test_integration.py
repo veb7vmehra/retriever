e5ed1c0ce866978aed057327b7c8743991334750
Jaffrey Joy <jaffreyjoy@gmail.com>
2019-03-18 05:10:28 +0530

    Change python encoding from latin-1 to UTF-8

--- test/test_integration.py
+++ test/test_integration.py
@@ -1,4 +1,4 @@
-# -*- coding: latin-1  -*-
+# -*- coding: UTF-8  -*-
 # """Integrations tests for Data Retriever"""
 from __future__ import print_function
 
@@ -128,7 +128,7 @@ data_no_header = {
 
 csv_latin1_encoding = {
     "name": "csv_latin1_encoding",
-    "raw_data": ["a,b,c", u"1,2,4L�ve", "4,5,6"],
+    "raw_data": ["a,b,c", u'1,2,4Löve', "4,5,6"],
     "script": {
         "name": "csv_latin1_encoding",
         "resources": [
@@ -144,7 +144,7 @@ csv_latin1_encoding = {
         "version": "1.0.0",
         "urls": {"csv_latin1_encoding": "http://example.com/csv_latin1_encoding.txt"},
     },
-    "expect_out": [u"a,b,c", u"1,2,4L�ve", u"4,5,6"],
+    "expect_out": [u"a,b,c", u'1,2,4Löve', u"4,5,6"],
 }
 
 autopk_csv = {
